077af9d5f9bda007aade17ea9e6e29dca7080a65
Mainak Nath <dn.mainak@gmail.com>
2026-01-02 22:54:54 +0530

    Fix multi-page routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Demo/',       // <--- Your Repo Name
+  base: '/Demo/',
   build: {
-    outDir: 'docs',     // <--- Output to "docs" instead of "dist"
-  }
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // ADD YOUR OTHER PAGES HERE:
+        page2: resolve(__dirname, 'page2.html'), 
+        page3: resolve(__dirname, 'page3.html'),
+      },
+    },
+  },
 })
